cf0fa4281261ef22d2365d86dc6616ab4f045dc5
Alak <alakd1985@gmail.com>
2023-03-10 17:46:46 -0500

    Implementation of Java Generics

--- src/test/java/apiTests/Endpoints.java
+++ src/test/java/apiTests/Endpoints.java
@@ -4,35 +4,38 @@ import apiEngine.AddBooksRequest;
 import apiEngine.AuthorizationRequest;
 import apiEngine.RemoveBookRequest;
 import apiEngine.Route;
+import apiEngine.model.responses.Books;
+import apiEngine.model.responses.Token;
+import apiEngine.model.responses.UserAccount;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Endpoints {
     private static final String BASE_URL = "https://bookstore.toolsqa.com";
-    public static Response authenticateUser(AuthorizationRequest authRequest) {
+    public static IRestResponse<Token> authenticateUser(AuthorizationRequest authRequest) {
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         Response response = request.body(authRequest).post(Route.generateToken());
-        return response;
+        return new RestResponse(Token.class, response);
     }
 
-    public static Response getBooks() {
+    public static IRestResponse<Books> getBooks() {
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         Response response = request.get(Route.books());
-        return response;
+        return new RestResponse(Books.class, response);
     }
 
-    public static Response addBook(AddBooksRequest addBooksRequest, String token) {
+    public static IRestResponse<UserAccount> addBook(AddBooksRequest addBooksRequest, String token) {
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
         request.header("Authorization", "Bearer " + token)
                 .header("Content-Type", "application/json");
         Response response = request.body(addBooksRequest).post(Route.books());
-        return response;
+        return new RestResponse(UserAccount.class, response);
     }
 
     public static Response removeBook(RemoveBookRequest removeBookRequest, String token) {
@@ -44,12 +47,12 @@ public class Endpoints {
         return response;
     }
 
-    public static Response getUserAccount(String userId, String token) {
+    public static IRestResponse<UserAccount> getUserAccount(String userId, String token) {
         RestAssured.baseURI = BASE_URL;
         RequestSpecification request = RestAssured.given();
         request.header("Authorization", "Bearer " + token)
                 .header("Content-Type", "application/json");
         Response response = request.get(Route.userAccount(userId));
-        return response;
+        return new RestResponse(UserAccount.class, response);
     }
 }
--- src/test/java/stepDefinitions/StepsE2E.java
+++ src/test/java/stepDefinitions/StepsE2E.java
@@ -8,6 +8,7 @@ import apiEngine.model.Book;
 import apiEngine.model.responses.Books;
 import apiEngine.model.responses.Token;
 import apiEngine.model.responses.UserAccount;
+import apiTests.IRestResponse;
 import org.junit.Assert;
 import apiTests.Endpoints;
 import io.cucumber.java.en.Given;
@@ -19,27 +20,26 @@ public class StepsE2E {
     private static final String USER_ID = "5cfbfc9f-8315-4b5b-83ba-05ae69db1001";
     private static Response response;
     private static Token tokenResponse;
+    private static IRestResponse<UserAccount> userAccountResponse;
     private static Book book;
 
     @Given("I am an authorized user")
     public void i_am_an_authorized_user() {
         AuthorizationRequest authRequest = new AuthorizationRequest("Alak", "Toma*1996");
-        response = Endpoints.authenticateUser(authRequest);
-        tokenResponse = response.getBody().as(Token.class);
+        tokenResponse = Endpoints.authenticateUser(authRequest).getBody();
     }
 
     @Given("A list of books are available")
     public void a_list_of_books_are_available() {
-        response = Endpoints.getBooks();
-        Books books = response.getBody().as(Books.class);
-        book = books.books.get(0);
+        IRestResponse<Books> booksResponse = Endpoints.getBooks();
+        book = booksResponse.getBody().books.get(0);
     }
 
     @When("I add a book to my reading list")
     public void i_add_a_book_to_my_reading_list() {
         ISBN isbn = new ISBN(book.isbn);
         AddBooksRequest addBooksRequest = new AddBooksRequest(USER_ID, isbn);
-        response = Endpoints.addBook(addBooksRequest, tokenResponse.token);
+        userAccountResponse = Endpoints.addBook(addBooksRequest, tokenResponse.token);
     }
 
     @When("I remove a book from my reading list")
@@ -50,11 +50,10 @@ public class StepsE2E {
 
     @Then("the book is removed")
     public void the_book_is_removed() {
-        response = Endpoints.getUserAccount(USER_ID, tokenResponse.token);
-        //System.out.println(response.getStatusCode());
-        Assert.assertEquals(200, response.getStatusCode());
-        UserAccount userAccount = response.getBody().as(UserAccount.class);
-        Assert.assertEquals(0, userAccount.books.size());
+        Assert.assertEquals(204, response.getStatusCode());
+        userAccountResponse = Endpoints.getUserAccount(USER_ID, tokenResponse.token);
+        Assert.assertEquals(200, userAccountResponse.getStatusCode());
+        Assert.assertEquals(0, userAccountResponse.getBody().books.size());
     }
 
 }
