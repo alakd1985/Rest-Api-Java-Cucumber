93209f9c40b5bd5fd8a6626363429f68dcdc3c81
Alak <alakd1985@gmail.com>
2023-03-10 18:17:41 -0500

    Refactoring for Request Headers

--- src/test/java/apiTests/Endpoints.java
+++ src/test/java/apiTests/Endpoints.java
@@ -10,48 +10,41 @@ import apiEngine.model.responses.UserAccount;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
 
 public class Endpoints {
-    private static final String BASE_URL = "https://bookstore.toolsqa.com";
-    public static IRestResponse<Token> authenticateUser(AuthorizationRequest authRequest) {
-        RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
+    private final RequestSpecification request;
+
+    public Endpoints(String baseUrl) {
+        RestAssured.baseURI = baseUrl;
+        request = RestAssured.given();
         request.header("Content-Type", "application/json");
-        Response response = request.body(authRequest).post(Route.generateToken());
-        return new RestResponse(Token.class, response);
     }
 
-    public static IRestResponse<Books> getBooks() {
-        RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
+    public void authenticateUser(AuthorizationRequest authRequest) {
+        Response response = request.body(authRequest).post(Route.generateToken());
+        if (response.statusCode() != HttpStatus.SC_OK)
+            throw new RuntimeException("Authentication Failed. Content of failed Response: " + response.toString() + " , Status Code : " + response.statusCode());
+
+        Token tokenResponse = response.body().jsonPath().getObject("$", Token.class);
+        request.header("Authorization", "Bearer " + tokenResponse.token);
+    }
+
+    public IRestResponse<Books> getBooks() {
         Response response = request.get(Route.books());
         return new RestResponse(Books.class, response);
     }
 
-    public static IRestResponse<UserAccount> addBook(AddBooksRequest addBooksRequest, String token) {
-        RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
+    public IRestResponse<UserAccount> addBook(AddBooksRequest addBooksRequest) {
         Response response = request.body(addBooksRequest).post(Route.books());
         return new RestResponse(UserAccount.class, response);
     }
 
-    public static Response removeBook(RemoveBookRequest removeBookRequest, String token) {
-        RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
-        Response response = request.body(removeBookRequest).delete(Route.book());
-        return response;
+    public Response removeBook(RemoveBookRequest removeBookRequest) {
+        return request.body(removeBookRequest).delete(Route.book());
     }
 
-    public static IRestResponse<UserAccount> getUserAccount(String userId, String token) {
-        RestAssured.baseURI = BASE_URL;
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
+    public IRestResponse<UserAccount> getUserAccount(String userId) {
         Response response = request.get(Route.userAccount(userId));
         return new RestResponse(UserAccount.class, response);
     }
--- src/test/java/stepDefinitions/StepsE2E.java
+++ src/test/java/stepDefinitions/StepsE2E.java
@@ -18,20 +18,21 @@ import io.restassured.response.Response;
 
 public class StepsE2E {
     private static final String USER_ID = "5cfbfc9f-8315-4b5b-83ba-05ae69db1001";
-    private static Response response;
-    private static Token tokenResponse;
-    private static IRestResponse<UserAccount> userAccountResponse;
-    private static Book book;
+    private Response response;
+    private IRestResponse<UserAccount> userAccountResponse;
+    private Book book;
+    private final String BaseUrl = "https://bookstore.toolsqa.com";
+    private Endpoints endPoints;
 
     @Given("I am an authorized user")
     public void i_am_an_authorized_user() {
+        endPoints = new Endpoints(BaseUrl);
         AuthorizationRequest authRequest = new AuthorizationRequest("Alak", "Toma*1996");
-        tokenResponse = Endpoints.authenticateUser(authRequest).getBody();
     }
 
     @Given("A list of books are available")
     public void a_list_of_books_are_available() {
-        IRestResponse<Books> booksResponse = Endpoints.getBooks();
+        IRestResponse<Books> booksResponse = endPoints.getBooks();
         book = booksResponse.getBody().books.get(0);
     }
 
@@ -39,21 +40,21 @@ public class StepsE2E {
     public void i_add_a_book_to_my_reading_list() {
         ISBN isbn = new ISBN(book.isbn);
         AddBooksRequest addBooksRequest = new AddBooksRequest(USER_ID, isbn);
-        userAccountResponse = Endpoints.addBook(addBooksRequest, tokenResponse.token);
+        userAccountResponse = endPoints.addBook(addBooksRequest);
     }
 
     @When("I remove a book from my reading list")
     public void i_remove_a_book_from_my_reading_list() {
         RemoveBookRequest removeBookRequest = new RemoveBookRequest(USER_ID, book.isbn);
-        response = Endpoints.removeBook(removeBookRequest, tokenResponse.token);
+                response = endPoints.removeBook(removeBookRequest);
     }
 
     @Then("the book is removed")
     public void the_book_is_removed() {
         Assert.assertEquals(204, response.getStatusCode());
-        userAccountResponse = Endpoints.getUserAccount(USER_ID, tokenResponse.token);
-        Assert.assertEquals(200, userAccountResponse.getStatusCode());
-        Assert.assertEquals(0, userAccountResponse.getBody().books.size());
+        
+                userAccountResponse = endPoints.getUserAccount(USER_ID);
+                Assert.assertEquals(200, userAccountResponse.getStatusCode());
     }
 
 }
